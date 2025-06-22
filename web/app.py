1ba2d79f630c2e12b6dbfb152daaeec87a99e9eb
Jason Betrue <jabetrue@pasadena.edu>
2025-06-22 16:42:25 -0700

    Fix host/port binding for Render

--- web/app.py
+++ web/app.py
@@ -27,4 +27,4 @@ def submit_score():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-	app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
