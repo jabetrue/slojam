7f3ff10e8d640465e0f654f327d9f420e470fa34
Jason Betrue <jabetrue@pasadena.edu>
2025-06-22 16:39:59 -0700

    Fix host/port binding for Render

--- web/app.py
+++ web/app.py
@@ -26,4 +26,5 @@ def submit_score():
     return jsonify({"status": "success", "received": data}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+	app.run(debug=True, host='0.0.0.0', port=port)
