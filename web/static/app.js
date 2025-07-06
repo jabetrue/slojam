0ada23d2158363d4454b1cbf49bb8530068e324f
Jason Betrue <jabetrue@pasadena.edu>
2025-07-05 21:59:10 -0700

    lock icons

--- web/static/app.js
+++ web/static/app.js
@@ -54,8 +54,8 @@ document.addEventListener("DOMContentLoaded", () => {
       const visibleSLOs = showOnlyRequired ? sloList.filter(s => s.required) : sloList;
       
       const statusIcon = isStudentComplete(student.name)
-        ? '<img src="/static/images/scored.png" alt="All required SLOs scored" title="All required SLOs scored" style="width: 1.2rem; margin-left: 0.5rem;">'
-        : '<img src="/static/images/notscored.png" alt="Required SLOs incomplete" title="Required SLOs incomplete" style="width: 1.1rem; margin-left: 0.5rem;">';
+        ? '<img src="/static/images/scored.png" alt="All required SLOs scored" title="All required SLOs scored" class="status-icon">'
+        : '<img src="/static/images/notscored.png" alt="Required SLOs incomplete" title="Required SLOs incomplete" class="status-icon">';
 
       div.innerHTML = `<h2>${student.name} ${statusIcon}</h2>` + visibleSLOs.map(slo => {
 
